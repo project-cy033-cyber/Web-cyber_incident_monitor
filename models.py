6505b8d065321bc1adeb9a3652e4449ee543fdac
Musharraf khan <159607503+CY033@users.noreply.github.com>
2024-09-10 22:21:09 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
 
 class Incident(db.Model):
     id = db.Column(db.Integer, primary_key=True)
